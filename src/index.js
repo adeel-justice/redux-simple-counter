13c98aebc98b4f0664c06e9f6e2198eaf3a39640
adeel rajwani <adeel.rajwani@utexas.edu>
2020-06-11 21:31:32 -0500

    created number clicks, and color counter change

--- src/index.js
+++ src/index.js
@@ -10,3 +10,20 @@ subscribe(() => render(getState()))
 // Dispatch the "INCREMENT" action every time the +1 button is pressed
 const incrementButton = document.getElementById('increment');
 incrementButton.addEventListener('click', e => dispatch({ type: "INCREMENT" }));
+const decrementButton = document.getElementById('decrement');
+decrementButton.addEventListener('click', e => dispatch({ type: "DECREMENT" }));
+
+//by +5
+
+const incrementFiveButton = document.getElementById('incrementFive');
+incrementFiveButton.addEventListener('click', e => dispatch({ type: "INCREMENT5" }));
+
+
+//by -5
+
+const decrementFiveButton = document.getElementById('decrementFive');
+decrementFiveButton.addEventListener('click', e => dispatch({ type: "DECREMENT5" }));
+
+//dropdown (color change)
+const dropDown = document.getElementById('colors');
+dropDown.addEventListener('change', e => dispatch({ type: "COLOR", color: e.currentTarget.value}));
--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,51 @@
 const initialState = {
-    value: 0
+    value: 0,
+    color: 'black'
 }
 
 const reducer = (state = initialState, action) => {
+    console.log(action)
     let { type } = action;
     if (type === "INCREMENT") {
         return {
             value: state.value + 1
         }
+
     }
+    else if (type === "DECREMENT") {
+        return {
+            value: state.value - 1
+        }
+
+    }
+
+    else if (type === "INCREMENT5") {
+        return {
+            value: state.value + 5
+        }
+
+    }
+
+    else if (type === "DECREMENT5") {
+        return {
+            value: state.value - 5
+        }
+
+    }
+
+    else if (type === "COLOR") { 
+        return {
+            value: state.value,
+            color: action.color
+        }
+
+    }
+
+    
     return state
+
 }
+
+
+
+//Updating reducer.js to include a new if-block for the new
